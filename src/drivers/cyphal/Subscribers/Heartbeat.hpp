dcaff2c64fe65bcf0d67ea37772ebc185e55e88d
DMontes98 <daniel.montestolon@slu.edu>
2024-02-21 13:12:09 -0600

    Added proper heartbeat handling in the Cyphal Heartbeat Subscriber

--- src/drivers/cyphal/Subscribers/Heartbeat.hpp
+++ src/drivers/cyphal/Subscribers/Heartbeat.hpp
@@ -37,6 +37,7 @@
  * Defines basic functionality of UAVCAN Heartbeat.1.0 subscription
  *
  * @author Peter van der Perk <peter.vanderperk@nxp.com>
+ * @author Daniel Montes <daniel.montestolon@slu.edu>
  */
 
 #pragma once
@@ -47,15 +48,24 @@
 
 #include "BaseSubscriber.hpp"
 
-class UavcanHeartbeatSubscriber : public UavcanBaseSubscriber
+#include <px4_platform_common/module_params.h>
+
+#include <uORB/Publication.hpp>
+#include <uORB/Subscription.hpp>
+#include <uORB/topics/mscs_status.h>
+#include <uORB/topics/parameter_update.h>
+
+class UavcanHeartbeatSubscriber : public UavcanBaseSubscriber, public ModuleParams
 {
 public:
-	UavcanHeartbeatSubscriber(CanardHandle &handle) :
-		UavcanBaseSubscriber(handle, "", "Heartbeat", 0) { };
+	UavcanHeartbeatSubscriber(CanardHandle &handle) : UavcanBaseSubscriber(handle, "", "Heartbeat", 0), ModuleParams(nullptr)
+	{
+
+	};
 
 	void subscribe() override
 	{
-		// Subscribe to heartbeat messages
+		/* Subscribe to Cyphal Heartbeat messages */
 		_canard_handle.RxSubscribe(CanardTransferKindMessage,
 					   uavcan_node_Heartbeat_1_0_FIXED_PORT_ID_,  // The fixed Subject-ID
 					   uavcan_node_Heartbeat_1_0_EXTENT_BYTES_,
@@ -68,11 +78,81 @@ public:
 	{
 		/* VARIABLE DEFINITIONS */
 		CanardNodeID heartbeat_node;
+		bool updated = false;
+		const hrt_abstime system_timestamp = hrt_absolute_time();
 
 		/* FUNCTION BODY */
-		/* Identify Heartbeat Message */
-		heartbeat_node = receive.metadata.remote_node_id;
-		PX4_INFO("Heartbeat Received from node [%hhu]", heartbeat_node);
-	};
+		/* Check if uORB has been updated */
+		if (_mscs_status_sub.updated()) {
+			/* Copy uORB MSCS Status into local buffer */
+			_mscs_status_sub.copy(&_mscs_status_msg);
+
+			/* Get all the possibly updated parameters */
+			_left_adp_node_id = _mscs_status_msg.left_adp_node_id;
+			_right_adp_node_id = _mscs_status_msg.right_adp_node_id;
+		}
+
+		/* Check if MSCS is active */
+		if (_mscs_status_msg.module_active == true) {
+			/* Deserialize Heartbeat Structure */
+			_heartbeat_message_buffer_size = uavcan_node_Heartbeat_1_0_EXTENT_BYTES_;
+
+			if (uavcan_node_Heartbeat_1_0_deserialize_(&_heartbeat_message, (uint8_t*) receive.payload, &_heartbeat_message_buffer_size) == 0) {
+				/* Get current timestamp */
+				_mscs_status_msg.timestamp = system_timestamp;
+
+				/* Identify Heartbeat Message */
+				heartbeat_node = receive.metadata.remote_node_id;
+
+				/* Save data to proper fields */
+				if (heartbeat_node == _left_adp_node_id) {
+
+					/* Set Left ADP as active */
+					_mscs_status_msg.left_adp_active = true;
+
+					/* Save all parameters to Status structure */
+					_mscs_status_msg.left_adp_last_heartbeat_timestamp = system_timestamp;
+					_mscs_status_msg.left_adp_uptime = _heartbeat_message.uptime;
+					_mscs_status_msg.left_adp_mode   = _heartbeat_message.mode.value;
+					_mscs_status_msg.left_adp_status = _heartbeat_message.vendor_specific_status_code;
+
+					updated = true;
+
+				} else if (heartbeat_node == _right_adp_node_id) {
+					/* Set Right ADP as active */
+					_mscs_status_msg.right_adp_active = true;
+
+					/* Save all parameters to Status structure */
+					_mscs_status_msg.right_adp_last_heartbeat_timestamp = system_timestamp;
+					_mscs_status_msg.right_adp_uptime = _heartbeat_message.uptime;
+					_mscs_status_msg.right_adp_mode   = _heartbeat_message.mode.value;
+					_mscs_status_msg.right_adp_status = _heartbeat_message.vendor_specific_status_code;
+
+					updated = true;
+
+				}
+
+				/* Publish uORB Message */
+				if (updated) {
+					_mscs_status_pub.publish(_mscs_status_msg);
+				}
+			}
+
+		}
+	}
+
+private:
+	/* Heartbeat Message Buffer */
+	uavcan_node_Heartbeat_1_0 _heartbeat_message{};
+	size_t _heartbeat_message_buffer_size;
+
+	/* uORB Publishers*/
+	mscs_status_s _mscs_status_msg{};
+	uORB::Subscription _mscs_status_sub{ORB_ID(mscs_status)};
+	uORB::Publication<mscs_status_s> _mscs_status_pub{ORB_ID(mscs_status)};
+
+	/* Node IDs */
+	CanardNodeID _left_adp_node_id;
+	CanardNodeID _right_adp_node_id;
 
 };
